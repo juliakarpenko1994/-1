ea1728595d5fd81bbe4252aa97ac1327d1eb6c0e
Julia <yuliabir@mail.ru>
2022-09-12 00:23:10 +0400

    ДЗ к уроку 1

---
+++ Program.cs
@@ -0,0 +1,189 @@
+﻿/*Задача 1: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее,
+ а какое меньшее.*/
+
+/*int a = 5;
+int b = 7;
+
+if (a > b)
+{
+    System.Console.WriteLine(a);
+}
+else
+{
+    if (b > a)
+    {
+
+        System.Console.WriteLine(b);
+    }
+    else
+    {
+        System.Console.WriteLine("Числа равны");
+        }
+}*/
+
+
+/*int a = 2;
+int b = 10;
+
+if (a > b)
+{
+    System.Console.WriteLine(a);
+}
+else
+{
+    if (b > a)
+    {
+
+        System.Console.WriteLine(b);
+    }
+    else
+    {
+        System.Console.WriteLine("Числа равны");
+        }
+}*/
+
+
+/*int a = -9;
+int b = -3;
+
+if (a > b)
+{
+    System.Console.WriteLine(a);
+}
+else
+{
+    if (b > a)
+    {
+
+        System.Console.WriteLine(b);
+    }
+    else
+    {
+        System.Console.WriteLine("Числа равны");
+        }
+}*/
+
+
+/*Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.*/
+
+
+/*int a = 2;
+int b = 3;
+int c = 7;
+
+int max = a;
+
+if (max < b)
+{
+    max = b;
+}
+if (max < c)
+{
+    max = c;
+}
+    System.Console.WriteLine(max);*/
+
+
+
+/*int a = 44;
+int b = 5;
+int c = 78;
+
+int max = a;
+
+if (max < b)
+{
+    max = b;
+}
+if (max < c)
+{
+    max = c;
+}
+System.Console.WriteLine(max);*/
+
+
+/*int a = 22;
+int b = 3;
+int c = 9;
+
+int max = a;
+
+if (max < b)
+{
+    max = b;
+}
+if (max < c)
+{
+    max = c;
+}
+System.Console.WriteLine(max);*/
+
+
+/*Задача 6: Напишите программу которая на вход принимает число и выдаёт, является ли число чётным
+ (делится ли оно на два без остатка).*/
+
+/*int a = 4;
+if (a % 2 == 0)
+{
+    System.Console.WriteLine("Четное");
+}
+else
+{
+    System.Console.WriteLine("Нечетное");
+}*/
+
+
+
+
+
+
+/*int a = -3;
+if (a % 2 == 0)
+{
+    System.Console.WriteLine("Четное");
+}
+else
+{
+    System.Console.WriteLine("Нечетное");
+}*/
+
+
+
+/*int a = 7;
+if (a % 2 == 0)
+{
+    System.Console.WriteLine("Четное");
+}
+else
+{
+    System.Console.WriteLine("Нечетное");
+}*/
+
+
+/*Задача 8: Напишите программу, которая на вход принимает число (N), 
+а на выходе показывает все чётные числа от 1 до N.*/
+
+
+
+/*int n = 5;
+int i = 1;
+
+while (i <= n)
+{
+    if (i % 2 == 0){
+        System.Console.WriteLine(i);
+    }
+    i++;
+}*/
+
+
+/*int n = 8;
+int i = 1;
+
+while (i <= n)
+{
+    if (i % 2 == 0){
+        System.Console.WriteLine(i);
+    }
+    i++;
+}*/
